992a42c8ed2aff86a1d1554047c22420041f0165
desmond <desmond.yao@buaa.edu.cn>
2022-02-27 23:11:37 +0800

    trt add resnet and yolov5

--- test/test_trt_nets.py
+++ test/test_trt_nets.py
@@ -8,9 +8,7 @@ import warnings
 
 import torchvision.models as models
 
-from bin.pytorch2caffe import Runner
-
-FUSE = True
+from bin.pytorch2trt import Runner
 
 os.makedirs('tmp', exist_ok=True)
 
@@ -19,7 +17,35 @@ def test_resnet18(shape = [1, 3, 224, 224], opset_version=9):
     runner = Runner("resnet18", net, shape, opset_version)
     runner.pyotrch_inference()
     runner.convert()
-    runner.caffe_inference()
+    runner.trt_inference()
+    runner.check_result()
+
+def test_yolov5(shape = [1, 3, 640, 640], opset_version=13):
+    import torch
+    net = torch.hub.load('ultralytics/yolov5', 'yolov5s', autoshape=False, pretrained=False, device=torch.device('cpu'))
+
+    class Identity(torch.nn.Module):
+        def __init__(self):
+            super(Identity, self).__init__()
+            
+        def forward(self, x):
+            for i in range(self.nl):
+                x[i] = self.m[i](x[i])
+                bs, _, ny, nx = x[i].shape
+                x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+       
+            return x
+    
+    name, _ = list(net.model.named_children())[-1]
+    identity = Identity()
+    detect = getattr(net.model, name)
+    identity.__dict__.update(detect.__dict__)
+    setattr(net.model, name, identity)
+
+    runner = Runner("yolov5", net, shape, opset_version)
+    runner.pyotrch_inference(generate_onnx=True)
+    runner.convert()
+    runner.trt_inference()
     runner.check_result()
 
 
