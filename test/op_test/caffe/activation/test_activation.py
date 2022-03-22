9363887c357b11a779653009367a031bdd29f7d8
desmond <desmond.yao@buaa.edu.cn>
2022-03-22 14:22:33 +0800

    add linear and update activation test case

--- test/op_test/caffe/activation/test_activation.py
+++ test/op_test/caffe/activation/test_activation.py
@@ -6,9 +6,9 @@ import warnings
 
 from bin.utils import CaffeBaseTester as Tester
 
-def test_Conv2d_basic(shape = [1, 3, 32, 32], opset_version=13):
-    model = torch.nn.Conv2d(3, 5, kernel_size=1, stride=1, padding=0)
-    Tester("Conv2d_basic", model, shape, opset_version)
+def test_LeakyRelu(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.LeakyReLU()
+    Tester("LeakyRelu", model, shape, opset_version)
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
