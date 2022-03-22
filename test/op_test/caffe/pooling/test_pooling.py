bfc9625a7c76e30a2665c97c3ec2d0cf8fc7f0c2
desmond <desmond.yao@buaa.edu.cn>
2022-03-22 12:10:29 +0800

    update pooling test

--- test/op_test/caffe/pooling/test_pooling.py
+++ test/op_test/caffe/pooling/test_pooling.py
@@ -6,9 +6,49 @@ import warnings
 
 from bin.utils import CaffeBaseTester as Tester
 
-def test_pooling(shape = [1, 1, 32, 32], opset_version=13):
+def test_AdaptiveAvgPool2d_1x1(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveAvgPool2d((1, 1))
+    Tester("AdaptiveAvgPool2d_1x1", model, shape, opset_version)
+
+def test_AdaptiveAvgPool2d_1x2(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveAvgPool2d((1, 2))
+    Tester("AdaptiveAvgPool2d_1x2", model, shape, opset_version)
+
+def test_AdaptiveAvgPool2d_2x1(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveAvgPool2d((2, 1))
+    Tester("AdaptiveAvgPool2d_2x1", model, shape, opset_version)
+
+def test_AdaptiveMaxPool2d_1x1(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveMaxPool2d((1, 1))
+    Tester("AdaptiveMaxPool2d_1x1", model, shape, opset_version)
+
+def test_AdaptiveMaxPool2d_1x2(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveMaxPool2d((1, 2))
+    Tester("AdaptiveMaxPool2d_1x2", model, shape, opset_version)
+
+def test_AdaptiveMaxPool2d_2x1(shape = [1, 3, 32, 32], opset_version=13):
+    model = torch.nn.AdaptiveMaxPool2d((2, 1))
+    Tester("AdaptiveMaxPool2d_2x1", model, shape, opset_version)    
+
+def test_AvgPool2d_without_ceil_mode(shape = [1, 1, 32, 32], opset_version=13):
+    model = torch.nn.AvgPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=False)
+    Tester("AvgPool2d_without_ceil_mode", model, shape, opset_version)    
+
+def test_AvgPool2d_with_ceil_mode(shape = [1, 1, 32, 32], opset_version=13):
+    model = torch.nn.AvgPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)
+    Tester("AvgPool2d_with_ceil_mode", model, shape, opset_version)   
+
+def test_MaxPool2d_without_ceil_mode(shape = [1, 1, 32, 32], opset_version=13):
+    model = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=False)
+    Tester("MaxPool2d_without_ceil_mode", model, shape, opset_version)    
+
+def test_MaxPool2d_with_ceil_mode(shape = [1, 1, 32, 32], opset_version=13):
+    model = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)
+    Tester("MaxPool2d_with_ceil_mode", model, shape, opset_version)  
+
+def test_MaxPool2d_with_return_indices(shape = [1, 1, 32, 32], opset_version=13):
     model = torch.nn.MaxPool2d(2, stride=2, return_indices=True)
-    tester = Tester("pooling", model, shape, opset_version)
+    Tester("MaxPool2d_with_return_indices", model, shape, opset_version)
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
