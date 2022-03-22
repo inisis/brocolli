f21de088afc51f17b237d5104ce16c979734ea3f
desmond <desmond.yao@buaa.edu.cn>
2022-03-22 13:01:09 +0800

    fix conv transpose bug and update test case

--- converter/pytorch/pytorch_caffe_parser.py
+++ converter/pytorch/pytorch_caffe_parser.py
@@ -1031,8 +1031,13 @@ class PytorchCaffeParser(Parser):
 
         layer.convolution_param.group = attr['group']
 
-        bias_name = '{0}.bias'.format(source_node.weights_name)
-        weights_name = '{0}.weight'.format(source_node.weights_name)
+        if source_node.weights_name == "":
+            bias_name = 'bias'
+            weights_name = 'weight'
+        else:
+            bias_name = '{0}.bias'.format(source_node.weights_name)
+            weights_name = '{0}.weight'.format(source_node.weights_name)
+
         weight = self.state_dict[weights_name]
 
         weight = weight.numpy()
