b588485e8c0a47c5ac1b51822bdd98729331d7e1
desmond <desmond.yao@buaa.edu.cn>
2022-03-24 22:22:49 +0800

    add TensorRTBaseTester

--- bin/utils.py
+++ bin/utils.py
@@ -1,11 +1,20 @@
-from bin.pytorch2caffe import Runner
-
+from bin.pytorch2caffe import Runner as CaffeRunner
+from bin.pytorch2trt import Runner as TensorRTRunner
 
 class CaffeBaseTester(object):
     def __init__(self, name, model, shape, opset_version):
         super(CaffeBaseTester, self).__init__()
-        self.runner = Runner(name, model, shape, opset_version)
+        self.runner = CaffeRunner(name, model, shape, opset_version)
         self.runner.pyotrch_inference()
         self.runner.convert()
         self.runner.caffe_inference()
+        self.runner.check_result()
+
+class TensorRTBaseTester(object):
+    def __init__(self, name, model, shape, opset_version):
+        super(TensorRTBaseTester, self).__init__()
+        self.runner = TensorRTRunner(name, model, shape, opset_version)
+        self.runner.pyotrch_inference()
+        self.runner.convert()
+        self.runner.trt_inference()
         self.runner.check_result()
