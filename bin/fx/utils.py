527d674bc71e62388e6aa546bdd4df4ba8565e1d
inisis <desmond.yao@buaa.edu.cn>
2022-07-18 15:57:47 +0800

    add fx2onnx tester

--- bin/fx/utils.py
+++ bin/fx/utils.py
@@ -7,3 +7,13 @@ class CaffeBaseTester(object):
         self.runner.convert()
         self.runner.caffe_inference()
         self.runner.check_result()
+
+class OnnxBaseTester(object):
+    def __init__(self, name, model, shape, opset_version):
+        super(OnnxBaseTester, self).__init__()
+        from bin.fx.pytorch2onnx import Runner as OnnxRunner
+        self.runner = OnnxRunner(name, model, shape, opset_version)
+        self.runner.pyotrch_inference()
+        self.runner.convert()
+        self.runner.onnx_inference()
+        self.runner.check_result()        
