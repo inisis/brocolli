91fe4a2eafb2db9fdc5acc57707820c4aac44e0d
inisis <desmond.yao@buaa.edu.cn>
2022-08-02 17:00:32 +0800

    add tests

---
+++ test/fx/test_onnx_ops.py
@@ -0,0 +1,16 @@
+import pytest
+import warnings
+import argparse
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    parser = argparse.ArgumentParser(description='Pytorch 2 Onnx operator test.')
+    parser.add_argument('--cov', help='foo help')
+    args = parser.parse_args()
+    if args.cov == '--cov':
+        cov = ['--cov', '--cov-report=html:tmp/onnx_report']
+    else:
+        cov = []
+
+    pytest.main(['-p', 'no:warnings', '-v', 'test/fx/op_test/onnx'] + cov)
