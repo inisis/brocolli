3d5812eeb0b91abfd4659b44f0054ceded375fac
inisis <desmond.yao@buaa.edu.cn>
2022-08-26 00:22:17 +0900

    update lineear test

--- test/fx/op_test/onnx/linear/test_linear.py
+++ test/fx/op_test/onnx/linear/test_linear.py
@@ -10,6 +10,10 @@ def test_Linear_basic(shape = [1, 3], opset_version=13):
     model = torch.nn.Linear(3, 5, bias=True)
     Tester("Linear_basic", model, shape, opset_version)
 
+def test_Linear_nobias(shape = [1, 3], opset_version=13):
+    model = torch.nn.Linear(3, 5, bias=False)
+    Tester("Linear_nobias", model, shape, opset_version)
+
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
