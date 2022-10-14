c63047f2812ada3c64d4818e1dd16886aa1137dc
inisis <desmond.yao@buaa.edu.cn>
2022-10-14 21:43:13 +0900

    2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="brocolli",
-    version="1.0.0",
+    version="2.0.0",
     author="desmond",
     author_email="desmond.yao@buaa.edu.cn",
     description="everything in pytorch",
