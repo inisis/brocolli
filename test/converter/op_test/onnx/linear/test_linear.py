f550f4dcb9c5d6a67e4752575281f6af8cb2c8b9
inisis <desmond.yao@buaa.edu.cn>
2022-12-03 19:32:12 +0800

    format code

--- test/converter/op_test/onnx/linear/test_linear.py
+++ test/converter/op_test/onnx/linear/test_linear.py
@@ -5,20 +5,18 @@ import warnings
 from brocolli.testing.common_utils import OnnxBaseTester as Tester
 
 
-def test_Linear_basic(
-    shape=[1, 3],
-):
-    model = torch.nn.Linear(3, 5, bias=True)
-    Tester("Linear_basic", model, shape)
-
-
-def test_Linear_nobias(
-    shape=[1, 3],
-):
-    model = torch.nn.Linear(3, 5, bias=False)
-    Tester("Linear_nobias", model, shape)
-
+class TestLinearClass:
+    @pytest.mark.parametrize("bias", (True, False))
+    def test_Linear(
+        self,
+        request,
+        bias,
+        shape=[1, 3],
+    ):
+        model = torch.nn.Linear(3, 5, bias=bias)
+        x = torch.rand(shape)
+        Tester(request.node.name, model, x)
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    pytest.main(["-p", "no:warnings", "-v", "test/op_test/onnx/linear/test_linear.py"])
+    pytest.main(["-p", "no:warnings", "-v", "test/converter/op_test/onnx/linear/test_linear.py"])
