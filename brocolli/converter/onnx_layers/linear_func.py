495d41a3bfcd212c7f741fb8e73e8a97a8feeb7f
inisis <desmond.yao@buaa.edu.cn>
2023-02-02 16:36:11 +0800

    fix linear bug and add test

--- brocolli/converter/onnx_layers/linear_func.py
+++ brocolli/converter/onnx_layers/linear_func.py
@@ -13,5 +13,13 @@ class LinearFunc(BaseLayer):
 
     def generate_node(self, name=None, params=None, attr_dict=None):
         if len(self._output_shape[0]) == 2:
-            gemm_layer = GemmFunc(self._source_node)
+            gemm_layer = GemmFunc(self._source_node, auto_gen=False)
+            gemm_layer.add_bottom_top()
+            if (
+                "bias" in self._source_node.kwargs
+                and self._source_node.kwargs["bias"] is not None
+            ):
+                bias_node = self._source_node.kwargs["bias"]
+                gemm_layer._in_names.append(bias_node.name)
+            gemm_layer.generate_node(self._source_node.name)
             self.node_post_process(gemm_layer)
--- test/converter/op_test/onnx/linear/test_linear.py
+++ test/converter/op_test/onnx/linear/test_linear.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import pytest
 import warnings
 
@@ -13,7 +14,37 @@ class TestLinearClass:
         bias,
         shape=(1, 3),
     ):
-        model = torch.nn.Linear(3, 5, bias=bias)
+        class Linear(torch.nn.Module):
+            def __init__(self, bias):
+                super(Linear, self).__init__()
+                self.weight = torch.nn.Parameter(torch.FloatTensor(4, 3))
+                self.bias = None
+                if bias:
+                    self.bias = torch.nn.Parameter(torch.FloatTensor(4))
+
+            def forward(self, x):
+                return F.linear(x, self.weight, self.bias)
+
+        model = Linear(bias=bias)
+        x = torch.rand(shape)
+        Tester(request.node.name, model, x)
+
+    @pytest.mark.parametrize("bias", (True, False))
+    def test_Linear_Module(
+        self,
+        request,
+        bias,
+        shape=(1, 3),
+    ):
+        class Linear(torch.nn.Module):
+            def __init__(self, bias):
+                super(Linear, self).__init__()
+                self.linear = torch.nn.Linear(3, 5, bias)
+
+            def forward(self, x):
+                return self.linear(x)
+
+        model = Linear(bias=bias)
         x = torch.rand(shape)
         Tester(request.node.name, model, x)
 
