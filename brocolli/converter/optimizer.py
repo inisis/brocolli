97eb1ad4c243eddc3f30b0a81a6ff5df73296a5d
inisis <desmond.yao@buaa.edu.cn>
2023-02-11 23:46:04 +0800

    optimizer registration

--- brocolli/converter/optimizer.py
+++ brocolli/converter/optimizer.py
@@ -1,5 +1,22 @@
 import contextlib
 import onnx_graphsurgeon as gs
+from collections import OrderedDict
+
+DEFAULT_FUSION_PATTERNS = OrderedDict()
+
+
+def register_fusion_pattern(pattern):
+    def insert(fn):
+        if pattern in DEFAULT_FUSION_PATTERNS.keys():
+            raise
+        DEFAULT_FUSION_PATTERNS[pattern] = fn
+        return fn
+
+    return insert
+
+
+def get_default_fusion_patterns():
+    return DEFAULT_FUSION_PATTERNS
 
 
 def graph_constant_fold_inplace(graph):
