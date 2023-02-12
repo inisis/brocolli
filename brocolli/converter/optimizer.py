689eb745c6dce8ab285c5bbff9586c51ab8a82ce
inisis <desmond.yao@buaa.edu.cn>
2023-02-12 23:34:49 +0800

    better fusion

--- brocolli/converter/optimizer.py
+++ brocolli/converter/optimizer.py
@@ -1,15 +1,16 @@
+from loguru import logger
 import contextlib
 import onnx_graphsurgeon as gs
-from collections import OrderedDict
+from collections import OrderedDict, Counter
 
 DEFAULT_FUSION_PATTERNS = OrderedDict()
 
 
-def register_fusion_pattern(pattern):
+def register_fusion_pattern(layer_type):
     def insert(fn):
-        if pattern in DEFAULT_FUSION_PATTERNS.keys():
+        if layer_type in DEFAULT_FUSION_PATTERNS.keys():
             raise
-        DEFAULT_FUSION_PATTERNS[pattern] = fn
+        DEFAULT_FUSION_PATTERNS[layer_type] = fn
         return fn
 
     return insert
@@ -27,7 +28,8 @@ def graph_constant_fold_inplace(graph):
             node.outputs.clear()
 
 
-def find_gelu_nodes(graph):
+@register_fusion_pattern("GELU")
+def find_gelu_nodes(node):
     # fmt: off
     '''
              x
@@ -43,36 +45,35 @@ def find_gelu_nodes(graph):
             Mul
     '''
     # fmt: on
-    out_nodes = []
-    for node in graph.nodes:
-        with contextlib.suppress(IndexError):
-            if node.op == "Mul":
-                if (
-                    node.i(0).op == "Mul"
-                    and node.i(0).i(1).op == "Add"
-                    and node.i(0).i(1).i(0).op == "Erf"
-                    and node.i(0).i(1).i(0).i(0).op == "Div"
-                ):
-                    input_variable = node.i(0).i(1).i(0).i(0).inputs[0]
-                    mul_node = node.i(0)
-                    div_node = node.i(0).i(1).i(0).i(0)
+    match = {}
+    if node.op == "Mul":
+        if (
+            node.i(0).op == "Mul"
+            and node.i(0).i(1).op == "Add"
+            and node.i(0).i(1).i(0).op == "Erf"
+            and node.i(0).i(1).i(0).i(0).op == "Div"
+        ):
+            input_variable = node.i(0).i(1).i(0).i(0).inputs[0]
+            mul_node = node.i(0)
+            div_node = node.i(0).i(1).i(0).i(0)
 
-                    input_variable.outputs.remove(mul_node)
-                    input_variable.outputs.remove(div_node)
+            input_variable.outputs.remove(mul_node)
+            input_variable.outputs.remove(div_node)
 
-                    output_variable = node.outputs[0]
-                    output_variable.inputs.clear()
-                    out_nodes += [
-                        {
-                            "inps": [input_variable],
-                            "outs": [output_variable],
-                        }
-                    ]
+            output_variable = node.outputs[0]
+            output_variable.inputs.clear()
+            match.update(
+                {
+                    "inputs": [input_variable],
+                    "outputs": [output_variable],
+                }
+            )
 
-    return out_nodes
+    return match
 
 
-def find_swish_nodes(graph):
+@register_fusion_pattern("Swish")
+def find_swish_nodes(node):
     # fmt: off
     '''
              x
@@ -82,31 +83,30 @@ def find_swish_nodes(graph):
             Mul
     '''
     # fmt: on
-    out_nodes = []
-    for node in graph.nodes:
-        with contextlib.suppress(IndexError):
-            if node.op == "Mul":
-                if node.i(1).op == "Sigmoid":
-                    input_variable = node.i(1).inputs[0]
-                    mul_node = node.i(1)
-                    sigmoid_node = node
+    match = {}
+    if node.op == "Mul":
+        if node.i(1).op == "Sigmoid":
+            input_variable = node.i(1).inputs[0]
+            mul_node = node.i(1)
+            sigmoid_node = node
 
-                    input_variable.outputs.remove(mul_node)
-                    input_variable.outputs.remove(sigmoid_node)
+            input_variable.outputs.remove(mul_node)
+            input_variable.outputs.remove(sigmoid_node)
 
-                    output_variable = node.outputs[0]
-                    output_variable.inputs.clear()
-                    out_nodes += [
-                        {
-                            "inps": [input_variable],
-                            "outs": [output_variable],
-                        }
-                    ]
+            output_variable = node.outputs[0]
+            output_variable.inputs.clear()
+            match.update(
+                {
+                    "inputs": [input_variable],
+                    "outputs": [output_variable],
+                }
+            )
 
-    return out_nodes
+    return match
 
 
-def find_layernorm_nodes(graph):
+@register_fusion_pattern("LayerNormalization")
+def find_layernorm_nodes(node):
     # fmt: off
     '''
              x
@@ -130,115 +130,99 @@ def find_layernorm_nodes(graph):
             Add
     '''
     # fmt: on
-    out_nodes = []
-    for node in graph.nodes:
-        with contextlib.suppress(IndexError):
-            if node.op == "Add":
-                if (
-                    node.i(0).op == "Mul"
-                    and node.i(0).i(0).op == "Div"
-                    and node.i(0).i(0).i(0).op == "Sub"
-                    and node.i(0).i(0).i(1).op == "Sqrt"
-                    and node.i(0).i(0).i(1).i(0).op == "Add"
-                    and node.i(0).i(0).i(1).i(0).i(0).op == "ReduceMean"
-                    and node.i(0).i(0).i(1).i(0).i(0).i(0).op == "Pow"
-                    and node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).op == "Sub"
-                    and node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1).op == "ReduceMean"
-                ):
-                    input_variable = (
-                        node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1).inputs[0]
-                    )
-                    sub_node = node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1)
-                    reducemean_node = node.i(0).i(0).i(1).i(0).i(0).i(0).i(0)
+    match = {}
+    if node.op == "Add":
+        if (
+            node.i(0).op == "Mul"
+            and node.i(0).i(0).op == "Div"
+            and node.i(0).i(0).i(0).op == "Sub"
+            and node.i(0).i(0).i(1).op == "Sqrt"
+            and node.i(0).i(0).i(1).i(0).op == "Add"
+            and node.i(0).i(0).i(1).i(0).i(0).op == "ReduceMean"
+            and node.i(0).i(0).i(1).i(0).i(0).i(0).op == "Pow"
+            and node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).op == "Sub"
+            and node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1).op == "ReduceMean"
+        ):
+            input_variable = node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1).inputs[0]
+            sub_node = node.i(0).i(0).i(1).i(0).i(0).i(0).i(0).i(1)
+            reducemean_node = node.i(0).i(0).i(1).i(0).i(0).i(0).i(0)
 
-                    input_variable.outputs.remove(sub_node)
-                    input_variable.outputs.remove(reducemean_node)
+            input_variable.outputs.remove(sub_node)
+            input_variable.outputs.remove(reducemean_node)
 
-                    weight_variable = node.i(0).inputs[1]
-                    bias_variable = node.inputs[1]
+            weight_variable = node.i(0).inputs[1]
+            bias_variable = node.inputs[1]
 
-                    output_variable = node.outputs[0]
-                    output_variable.inputs.clear()
-                    out_nodes += [
-                        {
-                            "inps": [
-                                input_variable,
-                                weight_variable,
-                                bias_variable,
-                            ],
-                            "outs": [output_variable],
-                            "attrs": {
-                                "attrs": str(
-                                    {
-                                        "axis": node.i(0)
-                                        .i(0)
-                                        .i(1)
-                                        .i(0)
-                                        .i(0)
-                                        .attrs["axes"][0],
-                                        "eps": float(
-                                            node.i(0).i(0).i(1).i(0).inputs[1].values
-                                        ),
-                                    }
-                                )
-                            },
-                        }
-                    ]
-    return out_nodes
+            output_variable = node.outputs[0]
+            output_variable.inputs.clear()
+            match.update(
+                {
+                    "inputs": [
+                        input_variable,
+                        weight_variable,
+                        bias_variable,
+                    ],
+                    "outputs": [output_variable],
+                    "attrs": {
+                        "attrs": str(
+                            {
+                                "axis": node.i(0).i(0).i(1).i(0).i(0).attrs["axes"][0],
+                                "eps": float(node.i(0).i(0).i(1).i(0).inputs[1].values),
+                            }
+                        )
+                    },
+                }
+            )
+    return match
 
 
 @gs.Graph.register()
-def replace_gelu(self, inputs, outputs, name):
+def replace_custom_layer(
+    self, op, inputs, outputs, name, attrs=None, domain="ai.onnx.contrib"
+):
     return self.layer(
-        op="GELU", inputs=inputs, outputs=outputs, name=name, domain="ai.onnx.contrib"
-    )
-
-
-@gs.Graph.register()
-def replace_swish(self, inputs, outputs, name):
-    return self.layer(
-        op="Swish", inputs=inputs, outputs=outputs, name=name, domain="ai.onnx.contrib"
-    )
-
-
-@gs.Graph.register()
-def replace_layernorm(self, inputs, outputs, attrs, name):
-    return self.layer(
-        op="LayerNormalization",
+        op=op,
         inputs=inputs,
         outputs=outputs,
         name=name,
         attrs=attrs,
-        domain="ai.onnx.contrib",
+        domain=domain,
     )
+
+
+def find_matches(graph, fusion_patterns):
+    match_map = {}
+    counter = Counter()
+    for node in reversed(graph.nodes):
+        if node.name not in match_map:
+            for layer_type, func in fusion_patterns.items():
+                with contextlib.suppress(IndexError):
+                    match = func(node)
+                    if match:
+                        logger.debug("matched patter {}", layer_type)
+                        match.update({"op": layer_type})
+                        match.update(
+                            {
+                                "name": "{}_{}".format(
+                                    layer_type.lower(), counter[layer_type]
+                                )
+                            }
+                        )
+                        counter.update([layer_type])
+                        # the first pattern matches will take precedence
+                        if node.name not in match_map:
+                            match_map[node.name] = match
+
+    return match_map
 
 
 def optimize_model(model):
     graph = gs.import_onnx(model)
     graph.fold_constants().cleanup()
-    gelu_nodes = find_gelu_nodes(graph)
-    swish_node = find_swish_nodes(graph)
-    layernorm_node = find_layernorm_nodes(graph)
-
-    for i, itn in enumerate(gelu_nodes):
-        inputs = itn["inps"]
-        outputs = itn["outs"]
-        name = "gelu_{}".format(i)
-        graph.replace_gelu(inputs, outputs, name)
-
-    for i, itn in enumerate(swish_node):
-        inputs = itn["inps"]
-        outputs = itn["outs"]
-        name = "swish_{}".format(i)
-        graph.replace_swish(inputs, outputs, name)
-
-    for i, itn in enumerate(layernorm_node):
-        inputs = itn["inps"]
-        outputs = itn["outs"]
-        attrs = itn["attrs"]
-        name = "layernorm_{}".format(i)
-        graph.replace_layernorm(inputs, outputs, attrs, name)
-
+    fusion_patterns = get_default_fusion_patterns()
+    fusion_pairs = find_matches(graph, fusion_patterns)
+    for _, match in fusion_pairs.items():
+        graph.replace_custom_layer(**match)
     graph_constant_fold_inplace(graph)
     graph.cleanup(remove_unused_graph_inputs=True).toposort()
     model = gs.export_onnx(graph)
