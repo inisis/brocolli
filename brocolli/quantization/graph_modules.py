754db67eb6c5fb1660678ca03edaa6a4cea6a2aa
inisis <desmond.yao@buaa.edu.cn>
2023-04-27 00:56:23 +0800

    fix graphmodule bug

--- brocolli/quantization/graph_modules.py
+++ brocolli/quantization/graph_modules.py
@@ -5,14 +5,18 @@ from torch.fx import GraphModule
 
 class BrocolliGraphModule(GraphModule):
     def __init__(self, root, graph, class_name="GraphModule"):
-        super().__init__(root, graph, class_name)
+        graph_copy = copy.deepcopy(graph)
+        super().__init__(root, graph_copy, class_name)
         self.class_name = class_name
 
     def __deepcopy__(self, memo):
         fake_mod = torch.nn.Module()
         fake_mod.__dict__ = copy.deepcopy(self.__dict__)
-        graph_copy = copy.deepcopy(self.graph)
-        graph_module = BrocolliGraphModule(fake_mod, graph_copy, self.class_name)
-        graph_module.__dict__.update(fake_mod.__dict__)
+        graph_module = BrocolliGraphModule(fake_mod, self.graph, self.class_name)
+        for key, value in fake_mod.__dict__.items():
+            if (
+                key not in graph_module.__dict__.keys()
+            ):  # to handle those user-defined attr
+                graph_module.__dict__[key] = value
 
         return graph_module
