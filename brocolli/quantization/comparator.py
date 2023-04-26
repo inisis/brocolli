5977cea75f26249a18f168de35fcef9e76590118
inisis <desmond.yao@buaa.edu.cn>
2023-04-26 23:10:52 +0800

    fix compare

--- brocolli/quantization/comparator.py
+++ brocolli/quantization/comparator.py
@@ -23,7 +23,7 @@ class FXComparator(Interpreter):
             if self.dynamic_batch:
                 shape[0] = -1
 
-            meta_info["tensor"] = result
+            meta_info["tensor"] = result.clone() # in case of inplace op
             meta_info["shape"] = torch.Size(shape)
             meta_info["dtype"] = result.dtype
 
@@ -46,3 +46,7 @@ class FXComparator(Interpreter):
 
     def __call__(self, x):
         return self.run(x)
+
+    def eval(self):
+        self.module.eval()
+        return self
