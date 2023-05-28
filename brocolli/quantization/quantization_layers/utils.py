90c983a2a37d31aa96bfb774f4929f0a95403ea1
inisis <desmond.yao@buaa.edu.cn>
2023-05-29 00:32:52 +0800

    update utils

--- brocolli/quantization/quantization_layers/utils.py
+++ brocolli/quantization/quantization_layers/utils.py
@@ -21,7 +21,6 @@ _quadruple = _ntuple(4)
 def _quantize_weight(float_wt, observer, bit_width=8):
     wt_scale = observer.calculate_qparams()
     wt_scale_tmp = torch.tensor(wt_scale)
-    wt_scale_tmp = wt_scale_tmp * 2 ** (bit_width - 1) / 2 ** (bit_width - 1)
     extra_dims = (1,) * (float_wt.dim() - 1)
     wt_scale_tmp = wt_scale_tmp.view(-1, *extra_dims)
     wt_scale_tmp = torch.where(
@@ -40,22 +39,13 @@ def _quantize_weight(float_wt, observer, bit_width=8):
     return qweight, wt_scale
 
 
-def _quantize_bias(float_bias, scale, bit_width=8):
-    bias_scale = torch.tensor(scale)
-    extra_dims = (1,) * (float_bias.dim() - 1)
-    bias_scale = bias_scale.view(-1, *extra_dims)
-    bias_scale_tmp = torch.where(
-        bias_scale == 0, torch.tensor(1.0, dtype=torch.float32), bias_scale
-    )
-    q_float_bias = torch.div(float_bias, bias_scale_tmp)
-    q_float_bias = torch.where(
-        bias_scale == 0, torch.tensor(0.0, dtype=torch.float32), q_float_bias
-    )
-    q_float_bias = torch.round(q_float_bias)
-    min_val = -(2 ** (bit_width - 1))
-    max_val = 2 ** (bit_width - 1) - 1
-    out_tensor = torch.clamp(q_float_bias, min_val, max_val)
-    qbias = out_tensor.to(torch.int64)
+def _quantize_bias(float_bias, scale, bit_width=32):
+    qmin = -(2.0 ** (bit_width - 1))
+    qmax = 2.0 ** (bit_width - 1) - 1
+
+    qbias = float_bias / scale
+    qbias = torch.clamp(qbias, qmin, qmax)
+    qbias = qbias.to(torch.int64)
 
     return qbias
 
