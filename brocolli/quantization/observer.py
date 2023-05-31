899c00fe9f9dd710675cbc7625b6ec1db646324c
inisis <desmond.yao@buaa.edu.cn>
2023-05-31 16:49:24 +0800

    update lsq

--- brocolli/quantization/observer.py
+++ brocolli/quantization/observer.py
@@ -1,6 +1,5 @@
-import warnings
 from abc import ABCMeta, abstractmethod
-from typing import Any, List, Tuple, Optional, Dict, Union
+from typing import Any
 from functools import partial
 import torch
 import torch.nn as nn
@@ -21,21 +20,6 @@ def get_available_observers():
 
 
 def _with_args(cls_or_self, **kwargs):
-    r"""Wrapper that allows creation of class factories.
-
-    This can be useful when there is a need to create classes with the same
-    constructor arguments, but different instances.
-
-    Example::
-
-        >>> Foo.with_args = classmethod(_with_args)
-        >>> foo_builder = Foo.with_args(a=3, b=4).with_args(answer=42)
-        >>> foo_instance1 = foo_builder()
-        >>> foo_instance2 = foo_builder()
-        >>> id(foo_instance1) == id(foo_instance2)
-        False
-    """
-
     class _PartialWrapper(object):
         def __init__(self, p):
             self.p = p
@@ -53,18 +37,6 @@ def _with_args(cls_or_self, **kwargs):
 
 
 class ObserverBase(ABC, nn.Module):
-    r"""Base observer Module.
-    Any observer implementation should derive from this class.
-
-    Concrete observers should follow the same API. In forward, they will update
-    the statistics of the observed Tensor. And they should provide a
-    `calculate_qparams` function that computes the quantization parameters given
-    the collected statistics.
-
-    Args:
-        dtype: Quantized data type
-    """
-
     def __init__(self, dtype):
         super(ObserverBase, self).__init__()
         self.dtype = dtype
@@ -89,52 +61,36 @@ class _ObserverBase(ObserverBase):
         self,
         dtype=torch.quint8,
         qscheme=torch.per_tensor_affine,
-        reduce_range=False,
-        quant_min=None,
-        quant_max=None,
         factory_kwargs=None,
     ):
         factory_kwargs = torch.nn.factory_kwargs(factory_kwargs)
         super(_ObserverBase, self).__init__(dtype=dtype)
         self.qscheme = qscheme
-        if reduce_range:
-            warnings.warn(
-                "Please use quant_min and quant_max to specify the range for observers. \
-                    reduce_range will be deprecated in a future release of PyTorch."
-            )
-        self.reduce_range = reduce_range
         self.register_buffer(
             "eps", torch.tensor([torch.finfo(torch.float32).eps], **factory_kwargs)
         )
-        assert self.qscheme in (
-            torch.per_tensor_affine,
-            torch.per_tensor_symmetric,
-            torch.per_channel_affine,
-            torch.per_channel_symmetric,
-            torch.per_channel_affine_float_qparams,
-        ), "Default Observer only works for per_tensor_affine, \
-                per_tensor_symmetric, per_channel_affine, \
-                per_channel_symmetric and per_channel_float_qparams quantization scheme"
-        assert self.dtype in (
-            torch.qint8,
-            torch.quint8,
-            torch.quint4x2,
-        ), "Default Observer only works for qint8, quint8 and quint4x2 data type"
-        self.has_customized_qrange = (quant_min is not None) and (quant_max is not None)
-        if self.has_customized_qrange:
-            self._validate_qmin_qmax(quant_min, quant_max)
-        self.quant_min = quant_min
-        self.quant_max = quant_max
         self.input_shape = None
+        self.lsq_enabled = False
 
-    @torch.jit.export
-    def _validate_qmin_qmax(self, quant_min: int, quant_max: int):
-        assert (
-            quant_min <= 0 <= quant_max
-        ), "Used-specified quantization range must include 0."
-        assert (
-            quant_min < quant_max
-        ), "qmin must be strictly less than qmax for user-specified quantization range."
+    def grad_scale(self, x, scale):
+        y = x
+        y_grad = x * scale
+        return (y - y_grad).detach() + y_grad
+
+    def round_pass(self, x):
+        y = x.round()
+        y_grad = x
+        return (y - y_grad).detach() + y_grad
+
+    def _lsq_forward(self, x):
+        s_grad_scale = 1.0 / ((self.quant_max * x.numel()) ** 0.5)
+        s_scale = self.grad_scale(self.lsq_scale, s_grad_scale)
+
+        x = x / s_scale
+        x = torch.clamp(x, self.quant_min, self.quant_max)
+        x = self.round_pass(x)
+        x = x * s_scale
+        return x
 
     @torch.jit.export
     def _calculate_qmin_qmax(self):
@@ -147,39 +103,6 @@ class _ObserverBase(ObserverBase):
 
     @torch.jit.export
     def _calculate_qparams(self, min_val: torch.Tensor, max_val: torch.Tensor):
-        r"""Calculates the quantization parameters, given min and max
-        value tensors. Works for both per tensor and per channel cases
-
-        Args:
-            min_val: Minimum values per channel
-            max_val: Maximum values per channel
-
-        Returns:
-            scales: Scales tensor of shape (#channels,)
-        """
-        if min_val.numel() == 0 or max_val.numel() == 0:
-            warnings.warn(
-                "must run observer before calling calculate_qparams.\
-                                    Returning default scale and zero point "
-            )
-            return torch.tensor([1.0]), torch.tensor([0])
-
-        if min_val.dim() == 0 or max_val.dim() == 0:
-            if min_val == float("inf") and max_val == float("-inf"):
-                warnings.warn(
-                    "must run observer before calling calculate_qparams.\
-                                        Returning default scale and zero point "
-                )
-                return torch.tensor([1.0]), torch.tensor([0])
-
-            assert min_val <= max_val, "min {} should be less than max {}".format(
-                min_val, max_val
-            )
-        else:
-            assert torch.all(
-                min_val <= max_val
-            ), "min {} should be less than max {}".format(min_val, max_val)
-
         quant_min, quant_max = self._calculate_qmin_qmax()
         min_val_neg = torch.min(min_val, torch.zeros_like(min_val))
         max_val_pos = torch.max(max_val, torch.zeros_like(max_val))
@@ -205,6 +128,14 @@ class _ObserverBase(ObserverBase):
 
         return scale
 
+    def enable_lsq(self):
+        self.lsq_scale = nn.Parameter(self.scale)
+        self.lsq_enabled = True
+
+    def disable_lsq(self):
+        self.lsq_scale = self.scale.detach().data
+        self.lsq_enabled = False
+
 
 @register_observer
 class MinMaxObserver(_ObserverBase):
@@ -215,33 +146,21 @@ class MinMaxObserver(_ObserverBase):
         self,
         dtype=torch.quint8,
         qscheme=torch.per_tensor_affine,
-        reduce_range=False,
-        quant_min=None,
-        quant_max=None,
         factory_kwargs=None,
     ):
         super(MinMaxObserver, self).__init__(
             dtype=dtype,
             qscheme=qscheme,
-            reduce_range=reduce_range,
-            quant_min=quant_min,
-            quant_max=quant_max,
             factory_kwargs=factory_kwargs,
         )
         factory_kwargs = torch.nn.factory_kwargs(factory_kwargs)
         self.register_buffer("min_val", torch.tensor(float("inf"), **factory_kwargs))
         self.register_buffer("max_val", torch.tensor(float("-inf"), **factory_kwargs))
-        if (
-            self.qscheme == torch.per_tensor_symmetric
-            and self.reduce_range
-            and self.dtype == torch.quint8
-        ):
-            raise NotImplementedError(
-                "Cannot reduce range for symmetric \
-                                       quantization for quint8"
-            )
 
     def forward(self, x_orig):
+        if self.lsq_enabled:
+            return self._lsq_forward(x_orig)
+
         self.input_shape = x_orig.shape
         if x_orig.numel() == 0:
             return x_orig
@@ -256,14 +175,16 @@ class MinMaxObserver(_ObserverBase):
 
     @torch.jit.export
     def calculate_qparams(self):
-        r"""Calculates the quantization parameters."""
-        quant_min, quant_max = self._calculate_qmin_qmax()
+        if self.lsq_enabled:
+            return self.lsq_scale
+
+        self.quant_min, self.quant_max = self._calculate_qmin_qmax()
         if self.min_val >= 0:
-            quant_min, quant_max = 0, 255
+            self.quant_min, self.quant_max = 0, 255
 
         max_val_pos = torch.max(torch.abs(self.min_val), self.max_val)
-        scale = max_val_pos / quant_max
-        return scale
+        self.scale = max_val_pos / self.quant_max
+        return self.scale
 
     @torch.jit.export
     def extra_repr(self):
@@ -280,31 +201,17 @@ class PerChannelMinMaxObserver(_ObserverBase):
         ch_axis=0,
         dtype=torch.quint8,
         qscheme=torch.per_channel_affine,
-        reduce_range=False,
-        quant_min=None,
-        quant_max=None,
         factory_kwargs=None,
     ):
         super(PerChannelMinMaxObserver, self).__init__(
             dtype=dtype,
             qscheme=qscheme,
-            reduce_range=reduce_range,
-            quant_min=quant_min,
-            quant_max=quant_max,
             factory_kwargs=factory_kwargs,
         )
         factory_kwargs = torch.nn.factory_kwargs(factory_kwargs)
         self.ch_axis = ch_axis
         self.register_buffer("min_vals", torch.tensor([], **factory_kwargs))
         self.register_buffer("max_vals", torch.tensor([], **factory_kwargs))
-        if (
-            self.qscheme == torch.per_channel_symmetric
-            and self.reduce_range
-            and self.dtype == torch.quint8
-        ):
-            raise NotImplementedError(
-                "Cannot reduce range for symmetric quantization for quint8"
-            )
 
     def forward(self, x_orig):
         self.input_shape = x_orig.shape
@@ -322,8 +229,6 @@ class PerChannelMinMaxObserver(_ObserverBase):
         new_axis_list[self.ch_axis] = 0
         new_axis_list[0] = self.ch_axis
         y = x.permute(new_axis_list)
-        # Need to match dtype of min/max because the updates to buffers
-        # are done in place and types need to match for comparisons
         y = y.to(self.min_vals.dtype)
         y = torch.flatten(y, start_dim=1)
         if min_vals.numel() == 0 or max_vals.numel() == 0:
@@ -344,71 +249,6 @@ class PerChannelMinMaxObserver(_ObserverBase):
 
     def extra_repr(self):
         return "min_val={}, max_val={}".format(self.min_vals, self.max_vals)
-
-
-def grad_scale(x, scale):
-    y = x
-    y_grad = x * scale
-    return (y - y_grad).detach() + y_grad
-
-
-def round_pass(x):
-    y = x.round()
-    y_grad = x
-    return (y - y_grad).detach() + y_grad
-
-
-class LSQObserver(_ObserverBase):
-    def __init__(
-        self, dtype=torch.qint8, all_positive=False, symmetric=False, per_channel=True
-    ):
-        super(LSQObserver, self).__init__(dtype)
-        bit = 8 if (dtype == torch.qint8 or dtype == torch.quint8) else 16
-        if all_positive:
-            assert not symmetric, "Positive quantization cannot be symmetric"
-            # unsigned activation is quantized to [0, 2^b-1]
-            self.thd_neg = 0
-            self.thd_pos = 2**bit - 1
-        else:
-            if symmetric:
-                # signed weight/activation is quantized to [-2^(b-1)+1, 2^(b-1)-1]
-                self.thd_neg = -(2 ** (bit - 1)) + 1
-                self.thd_pos = 2 ** (bit - 1) - 1
-            else:
-                # signed weight/activation is quantized to [-2^(b-1), 2^(b-1)-1]
-                self.thd_neg = -(2 ** (bit - 1))
-                self.thd_pos = 2 ** (bit - 1) - 1
-
-        self.per_channel = per_channel
-        self.s = torch.nn.Parameter(torch.ones(1))
-
-    def init_from(self, x, *args, **kwargs):
-        if self.per_channel:
-            self.s = torch.nn.Parameter(
-                x.detach().abs().mean(dim=list(range(1, x.dim())), keepdim=True)
-                * 2
-                / (self.thd_pos**0.5)
-            )
-        else:
-            self.s = torch.nn.Parameter(
-                x.detach().abs().mean() * 2 / (self.thd_pos**0.5)
-            )
-
-    def forward(self, x):
-        if self.per_channel:
-            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
-        else:
-            s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)
-        s_scale = grad_scale(self.s, s_grad_scale)
-
-        x = x / s_scale
-        x = torch.clamp(x, self.thd_neg, self.thd_pos)
-        x = round_pass(x)
-        x = x * s_scale
-        return x
-
-    def calculate_qparams(self):
-        return self.s
 
 
 default_observer = MinMaxObserver.with_args(qscheme=torch.per_tensor_symmetric)
